771300af9ff542064c5bef88f8f7b4cba91706ce
Ubuntu <ubuntu@ip-172-31-0-148.ap-northeast-2.compute.internal>
2018-01-19 17:56:05 +0000

    First commit

---
+++ src/commonmd.py
@@ -0,0 +1,86 @@
+import requests
+import time
+import datetime
+import logging
+import logging.handlers
+
+
+def getLogger( log_fpath, log_fname, logger_name, stream_mode = False ):
+	logger = logging.getLogger( logger_name )
+
+
+	file_max_bytes = 10 * 1024 * 1024
+	file_handler = logging.handlers.RotatingFileHandler( log_fpath + log_fname, 
+	                                                     maxBytes = file_max_bytes,
+																											 backupCount=2 )
+
+	formatter = logging.Formatter( '[%(levelname)s] - %(message)s' )
+	file_handler.setFormatter( formatter )
+	logger.addHandler( file_handler )
+
+	if stream_mode:
+		stream_handler = logging.StreamHandler()
+		stream_handler.setFormatter( formatter )
+		logger.addHandler( stream_handler )
+
+
+	logger.setLevel( logging.DEBUG )
+
+	return logger
+###/getLogger
+
+
+def printLogger( logger, levelname, log_date, message ):
+	if levelname == 'info':
+		new_message = '{0} > {1}'.format( log_date, message )
+		logger.info( new_message )
+	elif levelname == 'warn':
+		new_message = '{0} > {1}'.format( log_date, message )
+		logger.warn( new_message )
+	elif levelname == 'error':
+		new_message = '{0} > {1}'.format( log_date, message )
+		logger.error( new_message )
+	else:
+		new_message = '{0} > {1}'.format( log_date, message )
+		logger.debug( new_message )
+###/printLogger
+
+
+def getTicker(currency):
+	resource_url = 'https://api.bithumb.com/public/ticker/'
+	resource_uri = resource_url + currency
+
+	response = requests.get(resource_uri).json()
+
+	return response
+###/getTicker
+
+
+def getOrderbook(currency):
+	resource_url = 'https://api.bithumb.com/public/orderbook/'
+	resource_uri = resource_url + currency
+	
+	response = requests.get(resource_uri).json()
+	
+	return response
+###/getOrderbook
+
+
+def getRecentTransactions(currency):
+	resource_url = 'https://api.bithumb.com/public/recent_transactions/'
+	resource_uri = resource_url + currency
+	
+	response = requests.get(resource_uri).json()
+	
+	return response
+###/getRecentTransactions
+
+
+def getReadableDate( unixtime ):
+	unixtime += 32400 # 9시간 추가, 한국 시간으로 변경
+
+	file_date = datetime.datetime.fromtimestamp( int( unixtime ) ).strftime( '%Y%m%d' )[ 2: ]
+	log_date = datetime.datetime.fromtimestamp( int( unixtime ) ).strftime( '%Y%m%d %H:%M:%S' )[ 2: ]
+
+	return file_date, log_date
+###/getRecentDate
---
+++ src/crawler.py
@@ -0,0 +1,110 @@
+import commonmd as cmd
+import time
+import os
+import ujson
+
+
+def getFileWriters( file_date, coin_names ):
+	ticker_fpath       = '../ticker-data/{0}/'.format( file_date )
+	orderbook_fpath    = '../orderbook-data/{0}/'.format( file_date )
+	transactions_fpath = '../transactions-data/{0}/'.format( file_date )
+
+	if not os.path.isdir( ticker_fpath ):
+		os.mkdir( ticker_fpath )
+
+	if not os.path.isdir( orderbook_fpath ):
+		os.mkdir( orderbook_fpath )
+
+	if not os.path.isdir( transactions_fpath ):
+		os.mkdir( transactions_fpath )
+
+	ticker_fws       = dict()
+	orderbook_fws    = dict()
+	transactions_fws = dict()
+
+	for coin_name in coin_names:	
+		fname = '{0}.json'.format( coin_name )
+		ticker_fws[ coin_name ]       = open( ticker_fpath+fname, 'w', encoding='utf-8' )
+		orderbook_fws[ coin_name ]    = open( orderbook_fpath+fname, 'w', encoding='utf-8' )
+		transactions_fws[ coin_name ] = open( transactions_fpath+fname, 'w', encoding='utf-8' )
+
+	return ticker_fws, orderbook_fws, transactions_fws
+###/getFileWriters
+
+
+def closeFileWriters( ticker_fws, orderbook_fws, transactions_fws, coin_names ):
+	for coin_name in coin_names:
+		ticker_fws[ coin_name ].close()
+		orderbook_fws[ coin_name ].close()
+		transactions_fws[ coin_name ].close()
+#/closeFileWriters
+
+
+def main():
+	logger_name = 'crawler'
+	log_fpath   = '../logs/'
+	log_fname   = 'crawler.log'
+	stream_mode = False
+	logger = cmd.getLogger( log_fpath, log_fname, logger_name, stream_mode )
+
+	coin_names = [ 'BTC', 'ETH', 'DASH', 'LTC', 'ETC', 'XRP', 'BCH', 
+	               'XMR', 'ZEC', 'QTUM', 'BTG', 'EOS' ]
+
+	file_date, log_date = cmd.getReadableDate( time.time() )
+	cmd.printLogger( logger, 'info', log_date, 'Process start' )
+
+	ticker_fws, orderbook_fws, transactions_fws = getFileWriters( file_date, coin_names )
+
+	loop_cnt = 0
+	while True:
+		start_time = time.time()
+
+		loop_cnt += 1
+	
+		cur_file_date, log_date = cmd.getReadableDate( start_time )
+		if file_date != cur_file_date:
+			cmd.printLogger( logger, 'info', log_date, 
+			                 'Date updated({0} to {1})'.format( file_date, cur_file_date ) )
+			file_date = cur_file_date
+			closeFileWriters( ticker_fws, orderbook_fws, transactions_fws, coin_names )
+			ticker_fws, orderbook_fws, transactions_fws = getFileWriters( file_date, coin_names )
+
+		try: 
+			ticker_data    = cmd.getTicker( 'ALL' )
+			orderbook_data = cmd.getOrderbook( 'ALL' )
+
+			for coin_name in coin_names:
+				transactions_data = cmd.getRecentTransactions( coin_name )
+				transactions_data = ujson.dumps( transactions_data, ensure_ascii=False )
+				transactions_fws[ coin_name ].write( '{0}\n'.format( transactions_data ) )
+
+			for coin_name in coin_names:
+				sub_ticker_data = { 'status': ticker_data.get( 'status', -1 ),
+				                    'date': ticker_data.get( 'data', {} ).get( 'date', -1 ),
+														'data': ticker_data.get( 'data', {} ).get( coin_name, {} ) }
+				sub_ticker_data = ujson.dumps( sub_ticker_data, ensure_ascii=False )
+				ticker_fws[ coin_name ].write( '{0}\n'.format( sub_ticker_data ) )
+
+				sub_orderbook_data = { 'status': orderbook_data.get( 'status', -1 ),
+				                       'date': orderbook_data.get( 'data', {} ).get( 'timestamp', -1 ),
+														   'data': orderbook_data.get( 'data', {} ).get( coin_name, {} ) }
+				sub_orderbook_data = ujson.dumps( sub_orderbook_data, ensure_ascii=False )
+				orderbook_fws[ coin_name ].write( '{0}\n'.format( sub_orderbook_data ) )
+		except Exception as e:
+			cmd.printLogger( logger, 'error', log_date, e )
+
+		end_time = time.time()
+		time_gap = max( end_time - start_time, 0 )
+		if time_gap < 5:
+			time.sleep( 5 - time_gap )
+
+		if loop_cnt % 60 == 0:
+			cmd.printLogger( logger, 'info', log_date, 'Normal processing' )
+			loop_cnt = 0
+
+	closeFileWriters( ticker_fws, orderbook_fws, transactions_fws, coin_names )
+###/main
+
+
+if __name__ == '__main__':
+	main()
